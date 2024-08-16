4715b35f1ec8ab92556c1bc58bc5beadcce6fb22
Wolney Oliveira <wolney.f.oliveira@outlook.com>
2024-08-15 22:49:36 -0300

    feat: mapping properties

--- src/domain/entities/question.ts
+++ src/domain/entities/question.ts
@@ -1,11 +1,15 @@
-import { Slug } from "./value-objects/slug";
 import { Entity } from "../../core/entities/entity";
+import { UniqueEntityId } from "../../core/entities/unique-entity-id";
+import { Slug } from "./value-objects/slug";
 
 interface QuestionProps {
+  authorId: UniqueEntityId;
+  bestAnswerId?: UniqueEntityId;
   title: string;
-  slug: Slug;
   content: string;
-  authorId: string;
+  slug: Slug;
+  createdAt: Date;
+  updatedAt?: Date;
 }
 
 export class Question extends Entity<QuestionProps> {}
