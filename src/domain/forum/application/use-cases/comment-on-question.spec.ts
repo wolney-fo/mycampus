040fdc097bfa2f21676d47eb763b3f469d9e47e6
Wolney Oliveira <wolney.f.oliveira@outlook.com>
2024-09-09 23:51:32 -0300

    feat: create comment on question use case

---
+++ src/domain/forum/application/use-cases/comment-on-question.spec.ts
@@ -0,0 +1,38 @@
+import { makeQuestion } from "test/factories/make-question";
+import { InMemoryQuestionCommentsRepository } from "test/repositories/in-memory-question-comments-repository";
+import { InMemoryQuestionsRepository } from "test/repositories/in-memory-questions-repository";
+import { describe, it } from "vitest";
+import { CommentOnQuestionUseCase } from "./comment-on-question";
+
+let inMemoryQuestionsRepository: InMemoryQuestionsRepository;
+let inMemoryQuestionCommentsRepository: InMemoryQuestionCommentsRepository;
+let sut: CommentOnQuestionUseCase;
+
+describe("Comment on Question Use Case", () => {
+  beforeEach(() => {
+    inMemoryQuestionsRepository = new InMemoryQuestionsRepository();
+    inMemoryQuestionCommentsRepository =
+      new InMemoryQuestionCommentsRepository();
+
+    sut = new CommentOnQuestionUseCase(
+      inMemoryQuestionsRepository,
+      inMemoryQuestionCommentsRepository
+    );
+  });
+
+  it("should be able to comment on question", async () => {
+    const question = makeQuestion();
+
+    await inMemoryQuestionsRepository.create(question);
+
+    await sut.execute({
+      authorId: question.authorId.toString(),
+      questionId: question.id.toString(),
+      content: "New comment",
+    });
+
+    expect(inMemoryQuestionCommentsRepository.items[0].content).toEqual(
+      "New comment"
+    );
+  });
+});
