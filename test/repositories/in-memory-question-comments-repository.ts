c46b647cd5f5dcb6f818cac98056d08412174bd7
Wolney Oliveira <wolney.f.oliveira@outlook.com>
2024-09-12 22:22:47 -0300

    feat: create delete question comment use case

--- test/repositories/in-memory-question-comments-repository.ts
+++ test/repositories/in-memory-question-comments-repository.ts
@@ -9,4 +9,24 @@ export class InMemoryQuestionCommentsRepository
   async create(questionComment: QuestionComment) {
     this.items.push(questionComment);
   }
+
+  async findById(id: string) {
+    const questionComment = this.items.find(
+      (item) => item.id.toString() === id
+    );
+
+    if (!questionComment) {
+      return null;
+    }
+
+    return questionComment;
+  }
+
+  async delete(questionComment: QuestionComment) {
+    const itemIndex = this.items.findIndex(
+      (item) => item.id === questionComment.id
+    );
+
+    this.items.splice(itemIndex, 1);
+  }
 }
