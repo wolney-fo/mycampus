fbca33e7cc108bc4c88cdcd09339223fd48daccb
Wolney Oliveira <wolney.f.oliveira@outlook.com>
2024-10-03 22:23:52 -0300

    feat: questions can have attachments

--- src/domain/forum/application/use-cases/create-question.spec.ts
+++ src/domain/forum/application/use-cases/create-question.spec.ts
@@ -1,6 +1,7 @@
 import { InMemoryQuestionsRepository } from "test/repositories/in-memory-questions-repository";
 import { describe, it } from "vitest";
 import { CreateQuestionUseCase } from "./create-question";
+import { UniqueEntityId } from "@/core/entities/unique-entity-id";
 
 let inMemoryQuestionsRepository: InMemoryQuestionsRepository;
 let sut: CreateQuestionUseCase;
@@ -16,11 +17,17 @@ describe("Create Question Use Case", () => {
       authorId: "1",
       title: "New question",
       content: "Question content",
+      attachmentsIds: ["1", "2"],
     });
 
     expect(result.isRight()).toBe(true);
     expect(inMemoryQuestionsRepository.items[0]).toEqual(
       result.value?.question
     );
+    expect(inMemoryQuestionsRepository.items[0].attachments).toHaveLength(2);
+    expect(inMemoryQuestionsRepository.items[0].attachments).toEqual([
+      expect.objectContaining({ attachmentId: new UniqueEntityId("1") }),
+      expect.objectContaining({ attachmentId: new UniqueEntityId("2") }),
+    ]);
   });
 });
