d603b4b52640652e803a23256ff0cae086b8c1bd
Wolney Oliveira <wolney.f.oliveira@outlook.com>
2024-10-06 22:22:30 -0300

    feat: create new answer notification sending event

---
+++ src/domain/notification/application/subscribers/on-answer-created.spec.ts
@@ -0,0 +1,25 @@
+import { makeAnswer } from "test/factories/make-answer";
+import { OnAnswerCreated } from "./on-answer-created";
+import { InMemoryAnswersRepository } from "test/repositories/in-memory-answers-repository";
+import { InMemoryAnswerAttachmentsRepository } from "test/repositories/in-memory-answer-attachments-repository";
+
+let inMemoryAnswerAttachmentsRepository: InMemoryAnswerAttachmentsRepository;
+let inMemoryAnswersRepository: InMemoryAnswersRepository;
+
+describe("On Answer Created", () => {
+  beforeEach(() => {
+    inMemoryAnswerAttachmentsRepository =
+      new InMemoryAnswerAttachmentsRepository();
+    inMemoryAnswersRepository = new InMemoryAnswersRepository(
+      inMemoryAnswerAttachmentsRepository
+    );
+  });
+
+  it("should send a notification when an answer id created", async () => {
+    const onAnswerCreated = new OnAnswerCreated();
+
+    const answer = makeAnswer();
+
+    await inMemoryAnswersRepository.save(answer);
+  });
+});
--- test/repositories/in-memory-answers-repository.ts
+++ test/repositories/in-memory-answers-repository.ts
@@ -1,3 +1,4 @@
+import { DomainEvents } from "@/core/events/domain-events";
 import { PaginationParams } from "@/core/repositories/pagination-params";
 import { AnswerAttachmentsRepository } from "@/domain/forum/application/repositories/answer-attachments-repository";
 import { AnswersRepository } from "@/domain/forum/application/repositories/answers-repository";
@@ -36,6 +37,8 @@ export class InMemoryAnswersRepository implements AnswersRepository {
 
   async create(answer: Answer) {
     this.items.push(answer);
+
+    DomainEvents.dispatchEventsForAggregate(answer.id);
   }
 
   async delete(answer: Answer) {
