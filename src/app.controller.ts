a3e5320935c640c486ea6c0250977ae8149d07d1
Wolney Oliveira <wolney.f.oliveira@outlook.com>
2024-10-13 16:58:37 -0300

    feat: setup prisma orm

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,16 @@
 import { Controller, Get } from '@nestjs/common'
 import { AppService } from './app.service'
+import { PrismaService } from './prisma/prisma.service'
 
 @Controller()
 export class AppController {
-	constructor(private readonly appService: AppService) {}
+	constructor(
+		private appService: AppService,
+		private prisma: PrismaService
+	) {}
 
 	@Get()
-	getHello(): string {
-		return this.appService.getHello()
+	async getHello() {
+		return this.prisma.user.findFirst()
 	}
 }
