c170c8662abde930ea23738e421b2c67cf409306
Wolney Oliveira <wolney.f.oliveira@outlook.com>
2024-10-23 22:38:57 -0300

    test: setup e2e tests configurations

---
+++ test/setup-e2e.ts
@@ -0,0 +1,33 @@
+import 'dotenv/config'
+import { PrismaClient } from '@prisma/client'
+import { randomUUID } from 'node:crypto'
+import { execSync } from 'node:child_process'
+
+const prisma = new PrismaClient()
+
+function generateUniqueDatabaseURL(schemaId: string) {
+	if (!process.env.DATABASE_URL) {
+		throw new Error('Database URL not provided.')
+	}
+
+	const url = new URL(process.env.DATABASE_URL)
+
+	url.searchParams.set('schema', schemaId)
+
+	return url.toString()
+}
+
+const schemaID = randomUUID()
+
+beforeAll(async () => {
+	const databaseURL = generateUniqueDatabaseURL(schemaID)
+
+	process.env.DATABASE_URL = databaseURL
+
+	execSync('yarn prisma migrate deploy')
+})
+
+afterAll(async () => {
+	await prisma.$executeRawUnsafe(`DROP SCHEMA IF EXISTS "${schemaID}" CASCADE`)
+	await prisma.$disconnect()
+})
