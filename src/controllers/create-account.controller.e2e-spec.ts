c46fb85ec731c6a60a1de951b4c2077207d70a1a
Wolney Oliveira <wolney.f.oliveira@outlook.com>
2024-10-25 00:04:05 -0300

    test: create create account controller e2e test

--- src/controllers/create-account.controller.e2e-spec.ts
+++ src/controllers/create-account.controller.e2e-spec.ts
@@ -1,3 +1,40 @@
-it('Should be true', () => {
-	expect(1 + 1).toBe(2)
+import { AppModule } from '@/app.module'
+import { PrismaService } from '@/prisma/prisma.service'
+import { INestApplication } from '@nestjs/common'
+import { Test } from '@nestjs/testing'
+import request from 'supertest'
+
+describe('Create account (E2E)', () => {
+	let app: INestApplication
+	let prisma: PrismaService
+
+	beforeAll(async () => {
+		const moduleRef = await Test.createTestingModule({
+			imports: [AppModule],
+		}).compile()
+
+		app = moduleRef.createNestApplication()
+
+		prisma = moduleRef.get(PrismaService)
+
+		await app.init()
+	})
+
+	test('[POST] /accounts', async () => {
+		const response = await request(app.getHttpServer()).post('/accounts').send({
+			name: 'John Doe',
+			email: 'john@example.com',
+			password: '12345678',
+		})
+
+		expect(response.statusCode).toBe(201)
+
+		const userOnDatabase = await prisma.user.findUnique({
+			where: {
+				email: 'john@example.com',
+			},
+		})
+
+		expect(userOnDatabase).toBeTruthy()
+	})
 })
