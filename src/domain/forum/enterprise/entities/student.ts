8a4a6471bfe96dbc195c4407155acdb66f72bd54
Wolney Oliveira <wolney.f.oliveira@outlook.com>
2024-10-29 13:28:43 -0300

    feat: create register student use case

--- src/domain/forum/enterprise/entities/student.ts
+++ src/domain/forum/enterprise/entities/student.ts
@@ -1,14 +1,28 @@
-import { Entity } from "@/core/entities/entity";
-import { UniqueEntityId } from "@/core/entities/unique-entity-id";
+import { Entity } from '@/core/entities/entity'
+import { UniqueEntityId } from '@/core/entities/unique-entity-id'
 
 interface StudentProps {
-  name: string;
+	name: string
+	email: string
+	password: string
 }
 
 export class Student extends Entity<StudentProps> {
-  static create(props: StudentProps, id?: UniqueEntityId) {
-    const student = new Student(props, id);
+	get name() {
+		return this.props.name
+	}
 
-    return student;
-  }
+	get email() {
+		return this.props.email
+	}
+
+	get password() {
+		return this.props.password
+	}
+
+	static create(props: StudentProps, id?: UniqueEntityId) {
+		const student = new Student(props, id)
+
+		return student
+	}
 }
