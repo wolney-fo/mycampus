212ab5863014834ee28ccc90ff52bc62769f7d9c
Wolney Oliveira <wolney.f.oliveira@outlook.com>
2024-11-05 00:02:01 -0300

    refactor: create database level factories for question and student entities

--- test/factories/make-question.ts
+++ test/factories/make-question.ts
@@ -1,23 +1,44 @@
-import { UniqueEntityId } from "@/core/entities/unique-entity-id";
+import { faker } from '@faker-js/faker'
+
+import { UniqueEntityId } from '@/core/entities/unique-entity-id'
 import {
-  Question,
-  QuestionProps,
-} from "@/domain/forum/enterprise/entities/question";
-import { faker } from "@faker-js/faker";
+	Question,
+	QuestionProps,
+} from '@/domain/forum/enterprise/entities/question'
+import { PrismaService } from '@/infra/database/prisma/prisma.service'
+import { PrismaQuestionMapper } from '@/infra/database/prisma/mappers/prisma-question-mapper'
+import { Injectable } from '@nestjs/common'
 
 export function makeQuestion(
-  override: Partial<QuestionProps> = {},
-  id?: UniqueEntityId
-): Question {
-  const question = Question.create(
-    {
-      authorId: new UniqueEntityId(),
-      title: faker.lorem.sentence(),
-      content: faker.lorem.text(),
-      ...override,
-    },
-    id
-  );
+	override: Partial<QuestionProps> = {},
+	id?: UniqueEntityId
+) {
+	const question = Question.create(
+		{
+			authorId: new UniqueEntityId(),
+			title: faker.lorem.sentence(),
+			content: faker.lorem.text(),
+			...override,
+		},
+		id
+	)
 
-  return question;
+	return question
+}
+
+@Injectable()
+export class QuestionFactory {
+	constructor(private prisma: PrismaService) {}
+
+	async makePrismaQuestion(
+		data: Partial<QuestionProps> = {}
+	): Promise<Question> {
+		const question = makeQuestion(data)
+
+		await this.prisma.question.create({
+			data: PrismaQuestionMapper.toPrisma(question),
+		})
+
+		return question
+	}
 }
--- test/factories/make-student.ts
+++ test/factories/make-student.ts
@@ -1,14 +1,17 @@
 import { UniqueEntityId } from '@/core/entities/unique-entity-id'
 import {
-  Student,
-  StudentProps,
+	Student,
+	StudentProps,
 } from '@/domain/forum/enterprise/entities/student'
+import { PrismaStudentMapper } from '@/infra/database/prisma/mappers/prisma-student-mapper'
+import { PrismaService } from '@/infra/database/prisma/prisma.service'
 import { faker } from '@faker-js/faker'
+import { Injectable } from '@nestjs/common'
 
 export function makeStudent(
 	override: Partial<StudentProps> = {},
 	id?: UniqueEntityId
-): Student {
+) {
 	const student = Student.create(
 		{
 			name: faker.person.fullName(),
@@ -20,4 +23,19 @@ export function makeStudent(
 	)
 
 	return student
+}
+
+@Injectable()
+export class StudentFactory {
+	constructor(private prisma: PrismaService) {}
+
+	async makePrismaStudent(data: Partial<StudentProps> = {}): Promise<Student> {
+		const student = makeStudent(data)
+
+		await this.prisma.user.create({
+			data: PrismaStudentMapper.toPrisma(student),
+		})
+
+		return student
+	}
 }
