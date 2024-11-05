f9b153c7274395ce52c5daa06585cacadb1d8bed
Wolney Oliveira <wolney.f.oliveira@outlook.com>
2024-11-05 19:21:03 -0300

    feat: create delete question controller

--- src/domain/forum/application/use-cases/delete-question.ts
+++ src/domain/forum/application/use-cases/delete-question.ts
@@ -1,36 +1,39 @@
-import { Either, left, right } from "@/core/either";
-import { NotAllowedError } from "@/core/errors/errors/not-allowed-error";
-import { ResourceNotFoundError } from "@/core/errors/errors/resource-not-found-error";
-import { QuestionsRepository } from "../repositories/questions-repository";
+import { Either, left, right } from '@/core/either'
+import { NotAllowedError } from '@/core/errors/errors/not-allowed-error'
+import { ResourceNotFoundError } from '@/core/errors/errors/resource-not-found-error'
+import { QuestionsRepository } from '../repositories/questions-repository'
+import { Injectable } from '@nestjs/common'
 
 interface DeleteQuestionUseCaseRequest {
-  authorId: string;
-  questionId: string;
+	authorId: string
+	questionId: string
 }
 
 type DeleteQuestionUseCaseResponse = Either<
-  ResourceNotFoundError | NotAllowedError,
-  null
->;
+	ResourceNotFoundError | NotAllowedError,
+	null
+>
+
+@Injectable()
 export class DeleteQuestionUseCase {
-  constructor(private questionsRepository: QuestionsRepository) {}
+	constructor(private questionsRepository: QuestionsRepository) {}
 
-  async execute({
-    questionId,
-    authorId,
-  }: DeleteQuestionUseCaseRequest): Promise<DeleteQuestionUseCaseResponse> {
-    const question = await this.questionsRepository.findById(questionId);
+	async execute({
+		questionId,
+		authorId,
+	}: DeleteQuestionUseCaseRequest): Promise<DeleteQuestionUseCaseResponse> {
+		const question = await this.questionsRepository.findById(questionId)
 
-    if (!question) {
-      return left(new ResourceNotFoundError());
-    }
+		if (!question) {
+			return left(new ResourceNotFoundError())
+		}
 
-    if (authorId !== question.authorId.toString()) {
-      return left(new NotAllowedError());
-    }
+		if (authorId !== question.authorId.toString()) {
+			return left(new NotAllowedError())
+		}
 
-    await this.questionsRepository.delete(question);
+		await this.questionsRepository.delete(question)
 
-    return right(null);
-  }
+		return right(null)
+	}
 }
