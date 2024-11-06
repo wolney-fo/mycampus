8bef358f14e97fe6189767916eb522e1b0fd6396
Wolney Oliveira <wolney.f.oliveira@outlook.com>
2024-11-05 22:13:04 -0300

    feat: create delete question comment controller

--- src/domain/forum/application/use-cases/delete-question-comment.ts
+++ src/domain/forum/application/use-cases/delete-question-comment.ts
@@ -1,39 +1,40 @@
-import { Either, left, right } from "@/core/either";
-import { QuestionCommentsRepository } from "../repositories/question-comments-repository";
-import { NotAllowedError } from "@/core/errors/errors/not-allowed-error";
-import { ResourceNotFoundError } from "@/core/errors/errors/resource-not-found-error";
+import { Either, left, right } from '@/core/either'
+import { QuestionCommentsRepository } from '../repositories/question-comments-repository'
+import { NotAllowedError } from '@/core/errors/errors/not-allowed-error'
+import { ResourceNotFoundError } from '@/core/errors/errors/resource-not-found-error'
+import { Injectable } from '@nestjs/common'
 
 interface DeleteQuestionCommentUseCaseRequest {
-  authorId: string;
-  questionCommentId: string;
+	authorId: string
+	questionCommentId: string
 }
 
 type DeleteQuestionCommentUseCaseResponse = Either<
-  ResourceNotFoundError | NotAllowedError,
-  null
->;
+	ResourceNotFoundError | NotAllowedError,
+	null
+>
 
+@Injectable()
 export class DeleteQuestionCommentUseCase {
-  constructor(private questionCommentsRepository: QuestionCommentsRepository) {}
+	constructor(private questionCommentsRepository: QuestionCommentsRepository) {}
 
-  async execute({
-    authorId,
-    questionCommentId,
-  }: DeleteQuestionCommentUseCaseRequest): Promise<DeleteQuestionCommentUseCaseResponse> {
-    const questionComment = await this.questionCommentsRepository.findById(
-      questionCommentId
-    );
+	async execute({
+		authorId,
+		questionCommentId,
+	}: DeleteQuestionCommentUseCaseRequest): Promise<DeleteQuestionCommentUseCaseResponse> {
+		const questionComment =
+			await this.questionCommentsRepository.findById(questionCommentId)
 
-    if (!questionComment) {
-      return left(new ResourceNotFoundError());
-    }
+		if (!questionComment) {
+			return left(new ResourceNotFoundError())
+		}
 
-    if (questionComment.authorId.toString() !== authorId) {
-      return left(new NotAllowedError());
-    }
+		if (questionComment.authorId.toString() !== authorId) {
+			return left(new NotAllowedError())
+		}
 
-    await this.questionCommentsRepository.delete(questionComment);
+		await this.questionCommentsRepository.delete(questionComment)
 
-    return right(null);
-  }
+		return right(null)
+	}
 }
--- test/factories/make-question-comment.ts
+++ test/factories/make-question-comment.ts
@@ -1,20 +1,43 @@
-import { UniqueEntityId } from "@/core/entities/unique-entity-id";
-import { QuestionComment, QuestionCommentProps } from "@/domain/forum/enterprise/entities/question-comment";
-import { faker } from "@faker-js/faker";
+import { UniqueEntityId } from '@/core/entities/unique-entity-id'
+import {
+	QuestionComment,
+	QuestionCommentProps,
+} from '@/domain/forum/enterprise/entities/question-comment'
+import { PrismaQuestionCommentMapper } from '@/infra/database/prisma/mappers/prisma-question-comment-mapper'
+import { PrismaService } from '@/infra/database/prisma/prisma.service'
+import { faker } from '@faker-js/faker'
+import { Injectable } from '@nestjs/common'
 
 export function makeQuestionComment(
-  override: Partial<QuestionCommentProps> = {},
-  id?: UniqueEntityId
+	override: Partial<QuestionCommentProps> = {},
+	id?: UniqueEntityId
 ): QuestionComment {
-  const questionComment = QuestionComment.create(
-    {
-      authorId: new UniqueEntityId(),
-      questionId: new UniqueEntityId(),
-      content: faker.lorem.text(),
-      ...override,
-    },
-    id
-  );
+	const questionComment = QuestionComment.create(
+		{
+			authorId: new UniqueEntityId(),
+			questionId: new UniqueEntityId(),
+			content: faker.lorem.text(),
+			...override,
+		},
+		id
+	)
 
-  return questionComment;
+	return questionComment
+}
+
+@Injectable()
+export class QuestionCommentFactory {
+	constructor(private prisma: PrismaService) {}
+
+	async makePrismaQuestionComment(
+		data: Partial<QuestionCommentProps> = {}
+	): Promise<QuestionComment> {
+		const questionComment = makeQuestionComment(data)
+
+		await this.prisma.comment.create({
+			data: PrismaQuestionCommentMapper.toPrisma(questionComment),
+		})
+
+		return questionComment
+	}
 }
