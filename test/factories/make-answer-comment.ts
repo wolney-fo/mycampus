c35133f48dc42b184a9e3f4b018e676a77075e29
Wolney Oliveira <wolney.f.oliveira@outlook.com>
2024-11-05 22:33:42 -0300

    feat: create delete answer comment controller

--- test/factories/make-answer-comment.ts
+++ test/factories/make-answer-comment.ts
@@ -1,23 +1,43 @@
-import { UniqueEntityId } from "@/core/entities/unique-entity-id";
+import { UniqueEntityId } from '@/core/entities/unique-entity-id'
 import {
-  AnswerComment,
-  AnswerCommentProps,
-} from "@/domain/forum/enterprise/entities/answer-comment";
-import { faker } from "@faker-js/faker";
+	AnswerComment,
+	AnswerCommentProps,
+} from '@/domain/forum/enterprise/entities/answer-comment'
+import { PrismaAnswerCommentMapper } from '@/infra/database/prisma/mappers/prisma-answer-comment-mapper'
+import { PrismaService } from '@/infra/database/prisma/prisma.service'
+import { faker } from '@faker-js/faker'
+import { Injectable } from '@nestjs/common'
 
 export function makeAnswerComment(
-  override: Partial<AnswerCommentProps> = {},
-  id?: UniqueEntityId
+	override: Partial<AnswerCommentProps> = {},
+	id?: UniqueEntityId
 ): AnswerComment {
-  const answerComment = AnswerComment.create(
-    {
-      authorId: new UniqueEntityId(),
-      answerId: new UniqueEntityId(),
-      content: faker.lorem.text(),
-      ...override,
-    },
-    id
-  );
+	const answerComment = AnswerComment.create(
+		{
+			authorId: new UniqueEntityId(),
+			answerId: new UniqueEntityId(),
+			content: faker.lorem.text(),
+			...override,
+		},
+		id
+	)
 
-  return answerComment;
+	return answerComment
+}
+
+@Injectable()
+export class AnswerCommentFactory {
+	constructor(private prisma: PrismaService) {}
+
+	async makePrismaAnswerComment(
+		data: Partial<AnswerCommentProps> = {}
+	): Promise<AnswerComment> {
+		const answerComment = makeAnswerComment(data)
+
+		await this.prisma.comment.create({
+			data: PrismaAnswerCommentMapper.toPrisma(answerComment),
+		})
+
+		return answerComment
+	}
 }
