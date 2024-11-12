51e7d34f16c0df8594d3b797b8b4c9bdfa7032e2
Wolney Oliveira <wolney.f.oliveira@outlook.com>
2024-11-11 23:53:50 -0300

    test: create test for real storage service in upload attachment controller

--- src/domain/forum/application/repositories/attachments-repository.ts
+++ src/domain/forum/application/repositories/attachments-repository.ts
@@ -1,5 +1,5 @@
 import { Attachment } from '../../enterprise/entities/attachment'
 
 export abstract class AttachmentsRepository {
-	abstract create(attachments: Attachment): Promise<void>
+	abstract create(attachment: Attachment): Promise<void>
 }
--- src/infra/database/database.module.ts
+++ src/infra/database/database.module.ts
@@ -14,6 +14,8 @@ import { PrismaQuestionAttachmentsRepository } from './prisma/repositories/prism
 import { PrismaQuestionCommentsRepository } from './prisma/repositories/prisma-question-comments-repository'
 import { PrismaQuestionsRepository } from './prisma/repositories/prisma-questions-repository'
 import { PrismaStudentsRepository } from './prisma/repositories/prisma-students-repository'
+import { AttachmentsRepository } from '@/domain/forum/application/repositories/attachments-repository'
+import { PrismaAttachmentsRepository } from './prisma/repositories/prisma-attachments-repository'
 
 @Module({
 	providers: [
@@ -46,6 +48,10 @@ import { PrismaStudentsRepository } from './prisma/repositories/prisma-students-
 			provide: StudentsRepository,
 			useClass: PrismaStudentsRepository,
 		},
+		{
+			provide: AttachmentsRepository,
+			useClass: PrismaAttachmentsRepository,
+		},
 	],
 	exports: [
 		PrismaService,
@@ -56,6 +62,7 @@ import { PrismaStudentsRepository } from './prisma/repositories/prisma-students-
 		QuestionAttachmentsRepository,
 		QuestionsRepository,
 		StudentsRepository,
+		AttachmentsRepository,
 	],
 })
 export class DatabaseModule {}
